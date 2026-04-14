5a8b8ebaeadaef25aac4a9f7e1069676c01d6b5f
ZMC-Zero <129792568+Lutziano888@users.noreply.github.com>
2026-04-14 16:03:19 +0200

    Added forward declarations for texteditor functions.

---
+++ filemanager.c
@@ -0,0 +1,6 @@
+// Forward declarations for texteditor functions
+void texteditor_open(const char *filename);
+void texteditor_save(const char *filename);
+void texteditor_close();
+
+// Existing code of filemanager.c
